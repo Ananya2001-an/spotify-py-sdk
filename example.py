6d73ab35f3f1571730b45074bda2d425ef1172f2
Kanishk Pachauri <itskanishkp.py@gmail.com>
2024-01-02 06:18:47 +0530

    :memo: Docs: Added example for async sdk method request

--- example.py
+++ example.py
@@ -1,22 +1,29 @@
+import asyncio
 import os
 
 from src import SpotifyApi, SdkConfig
 from dotenv import load_dotenv
 load_dotenv()
 
+config = SdkConfig()
+config.before_request = lambda x, y: print("⁠✯🧸🎧☕⁠✯")
+api: SpotifyApi = SpotifyApi(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"), config)
+def main_sync():
 
-def main():
-    config = SdkConfig()
-    config.before_request = lambda x, y: print("⁠✯🧸🎧☕⁠✯")
     # TODO async/await request
     # config.after_request = lambda x, y, z: print("After request!")
 
-    api: SpotifyApi = SpotifyApi(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"), config)
+
     # print(api.albums.get(["4EcfbzCtbJDk2wMwhT4D1h", "4EcfbzCtbJDk2wMwhT4D1h"]))
     # print(api.current_user.followed_artists()) # insufficient client scope;
     # print(api.search.execute("purpose", ["album"]))
-    # print(api.browse.get_categories())
+    resp = api.browse.get_categories()
+    print(resp)
 
+async  def main_async():
+    async_response = await api.browse.get_categories()
+    print(async_response)
 
 if __name__ == "__main__":
-    main()
+    # main_sync()
+    asyncio.run(main_async())
