46357249eb029f2a6a20d62f5db690940ca2877f
Kanishk Pachauri <itskanishkp.py@gmail.com>
2024-01-02 09:25:45 +0530

    :white_check_mark: Test: add async test for users endpoint

--- tests/endpoints/test_users.py
+++ tests/endpoints/test_users.py
@@ -25,4 +25,9 @@ def test_get_user_profile(api, get_user_data):
     assert result["id"] == get_user_data["id"]
     assert result["display_name"] == get_user_data["display_name"]
 
+@pytest.mark.asyncio
+async def test_get_user_profile_async(api, get_user_data):
+    result = await api.users.profile(get_user_data["id"])
 
+    assert result["id"] == get_user_data["id"]
+    assert result["display_name"] == get_user_data["display_name"]
