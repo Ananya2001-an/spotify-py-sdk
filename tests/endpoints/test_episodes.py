1c020507f635b4290deeff5b2c2503bf01522c6f
Kanishk Pachauri <itskanishkp.py@gmail.com>
2024-01-02 09:15:56 +0530

    :white_check_mark: Test: add async test for episodes endpoint

--- tests/endpoints/test_episodes.py
+++ tests/endpoints/test_episodes.py
@@ -24,6 +24,11 @@ def test_get_episode_single_item(api, get_episode_data):
 
     assert result == get_episode_data
 
+@pytest.mark.asyncio
+async def test_get_episode_single_item_async(api, get_episode_data):
+    result = await api.episodes.get(get_episode_data["id"], "GB")
+
+    assert result == get_episode_data
 
 def test_get_episode_multiple_items(api, get_episode_data):
     result = api.episodes.get([get_episode_data["id"], get_episode_data["id"]], "GB")
@@ -32,4 +37,10 @@ def test_get_episode_multiple_items(api, get_episode_data):
     assert result[0]["id"] == get_episode_data["id"]
     assert result[1]["id"] == get_episode_data["id"]
 
+@pytest.mark.asyncio
+async def test_get_episode_multiple_items_async(api, get_episode_data):
+    result = await api.episodes.get([get_episode_data["id"], get_episode_data["id"]], "GB")
 
+    assert len(result) == 2
+    assert result[0]["id"] == get_episode_data["id"]
+    assert result[1]["id"] == get_episode_data["id"]
