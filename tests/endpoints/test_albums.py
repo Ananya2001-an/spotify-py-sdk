7ea54b9c17a0f3b0b40dcef5c85348d2fa5396ef
Kanishk Pachauri <itskanishkp.py@gmail.com>
2024-01-02 07:10:54 +0530

    :white_check_mark: Test: add async test for albums

--- tests/endpoints/test_albums.py
+++ tests/endpoints/test_albums.py
@@ -4,6 +4,8 @@ import pytest
 from src import SpotifyApi
 import os
 from dotenv import load_dotenv
+import asyncio
+
 load_dotenv()
 
 
@@ -32,6 +34,13 @@ def test_get_album_single_item(api, get_album_data):
     assert result["name"] == get_album_data["name"]
 
 
+@pytest.mark.asyncio
+async def test_get_album_single_item_async(api, get_album_data):
+    result = await api.albums.get(get_album_data["id"])
+
+    assert result["name"] == get_album_data["name"]
+
+
 def test_get_album_multiple_items(api, get_album_data):
     result = api.albums.get([get_album_data["id"], get_album_data["id"]])
 
@@ -40,7 +49,23 @@ def test_get_album_multiple_items(api, get_album_data):
     assert result[1]["id"] == get_album_data["id"]
 
 
+@pytest.mark.asyncio
+async def test_get_album_multiple_items_async(api, get_album_data):
+    result = await api.albums.get([get_album_data["id"], get_album_data["id"]])
+
+    assert len(result) == 2
+    assert result[0]["id"] == get_album_data["id"]
+    assert result[1]["id"] == get_album_data["id"]
+
+
 def test_get_album_tracks(api, get_album_data, get_album_tracks_data):
     result = api.albums.tracks(get_album_data["id"])
+
+    assert len(result["items"]) == len(get_album_tracks_data["items"])
+
+
+@pytest.mark.asyncio
+async def test_get_album_tracks_async(api, get_album_data, get_album_tracks_data):
+    result = await api.albums.tracks(get_album_data["id"])
 
     assert len(result["items"]) == len(get_album_tracks_data["items"])
