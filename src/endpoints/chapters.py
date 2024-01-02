47ed68697fbe6c2bb011e8de57dccca92c6868e3
Kanishk Pachauri <itskanishkp.py@gmail.com>
2024-01-02 08:02:42 +0530

    :sparkles: Feat: added support for async in chapter endpoint

--- src/endpoints/chapters.py
+++ src/endpoints/chapters.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from .endpoints_base import EndpointsBase
 from src.types import *
-
+import asyncio
 
 class Chapters(EndpointsBase):
     """Make calls to the chapters api endpoint
@@ -12,16 +12,31 @@ class Chapters(EndpointsBase):
         """
         super().__init__(api)
 
-    def get(self, id_or_ids: Union[str, list[str]], market: CHAPTER_MARKET):
-        """Get chapters
+    async def _get_async(self, id_or_ids: Union[str, list[str]], market: CHAPTER_MARKET):
+        """Asynchronously get chapters"""
+        if isinstance(id_or_ids, str):
+            params = EndpointsBase.params_for({"market": market})
+            return await self.get_request(f"chapters/{id_or_ids}{params}")
 
-        :param id_or_ids: pass single id or a list of ids
-        :param market: pass market like "GB"
-        """
-        if type(id_or_ids) is str:
+        params = EndpointsBase.params_for({"ids": id_or_ids, "market": market})
+        response = await self.get_request(f"chapters{params}")
+        return response["chapters"]
+
+    def _get_sync(self, id_or_ids: Union[str, list[str]], market: CHAPTER_MARKET):
+        """Synchronously get chapters"""
+        if isinstance(id_or_ids, str):
             params = EndpointsBase.params_for({"market": market})
             return self.get_request(f"chapters/{id_or_ids}{params}")
 
         params = EndpointsBase.params_for({"ids": id_or_ids, "market": market})
         response = self.get_request(f"chapters{params}")
         return response["chapters"]
+
+    def get(self, id_or_ids: Union[str, list[str]], market: CHAPTER_MARKET):
+        """Get chapters"""
+        if asyncio.get_event_loop().is_running():
+            return asyncio.ensure_future(self._get_async(id_or_ids, market))
+        else:
+            return self._get_sync(id_or_ids, market)
+
+
