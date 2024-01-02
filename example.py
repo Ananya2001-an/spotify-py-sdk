a79d7ebd53b3c39f62d3996835566a1b1c50f863
Kanishk Pachauri <itskanishkp.py@gmail.com>
2024-01-02 06:25:08 +0530

    :monocle_face: Chore: removed todo for async in example.py

--- example.py
+++ example.py
@@ -9,11 +9,7 @@ config = SdkConfig()
 config.before_request = lambda x, y: print("⁠✯🧸🎧☕⁠✯")
 api: SpotifyApi = SpotifyApi(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"), config)
 def main_sync():
-
-    # TODO async/await request
     # config.after_request = lambda x, y, z: print("After request!")
-
-
     # print(api.albums.get(["4EcfbzCtbJDk2wMwhT4D1h", "4EcfbzCtbJDk2wMwhT4D1h"]))
     # print(api.current_user.followed_artists()) # insufficient client scope;
     # print(api.search.execute("purpose", ["album"]))
