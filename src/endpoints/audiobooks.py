a219e6718c7aec4a5b443fd4adaea632f821abdb
Kanishk Pachauri <itskanishkp.py@gmail.com>
2024-01-02 07:16:28 +0530

    :sparkles: Feat: async support for audiobook endpoint

--- src/endpoints/audiobooks.py
+++ src/endpoints/audiobooks.py
@@ -1,6 +1,7 @@
 from .endpoints_base import EndpointsBase
 from typing import Optional, Union
 from src.types import *
+import asyncio
 
 
 class Audiobooks(EndpointsBase):
@@ -11,12 +12,19 @@ class Audiobooks(EndpointsBase):
         """
         super().__init__(api)
 
-    def get(self, id_or_ids: Union[str, list[str]], market: Optional[MARKET] = None):
-        """Get audiobooks by providing id
+    async def _get_async(self, id_or_ids: Union[str, list[str]], market: Optional[MARKET] = None):
+        """Asynchronously get audiobooks by providing id"""
+        if isinstance(id_or_ids, str):
+            params = EndpointsBase.params_for({"market": market})
+            return await self.get_request(f"audiobooks/{id_or_ids}{params}")
 
-        :param id_or_ids: pass id or list of ids
-        """
-        if type(id_or_ids) is str:
+        params = EndpointsBase.params_for({"ids": id_or_ids, "market": market})
+        response = await self.get_request(f"audiobooks{params}")
+        return response["audiobooks"]
+
+    def _get_sync(self, id_or_ids: Union[str, list[str]], market: Optional[MARKET] = None):
+        """Synchronously get audiobooks by providing id"""
+        if isinstance(id_or_ids, str):
             params = EndpointsBase.params_for({"market": market})
             return self.get_request(f"audiobooks/{id_or_ids}{params}")
 
@@ -24,10 +32,31 @@ class Audiobooks(EndpointsBase):
         response = self.get_request(f"audiobooks{params}")
         return response["audiobooks"]
 
-    def get_audiobook_chapters(self, id: str, market: Optional[MARKET] = None, limit: Optional[int] = None, offset: Optional[int] = None):
-        """Get chapters of an audiobook
+    def get(self, id_or_ids: Union[str, list[str]], market: Optional[MARKET] = None):
+        """Get audiobooks by providing id"""
+        if asyncio.get_event_loop().is_running():
+            return asyncio.ensure_future(self._get_async(id_or_ids, market))
+        else:
+            return self._get_sync(id_or_ids, market)
 
-        :param id: pass audiobook id
-        """
+        # Chapters method
+
+    async def _get_audiobook_chapters_async(self, id: str, market: Optional[MARKET] = None, limit: Optional[int] = None,
+                                            offset: Optional[int] = None):
+        """Asynchronously get chapters of an audiobook"""
+        params = EndpointsBase.params_for({"market": market, "limit": limit, "offset": offset})
+        return await self.get_request(f"audiobooks/{id}/chapters{params}")
+
+    def _get_audiobook_chapters_sync(self, id: str, market: Optional[MARKET] = None, limit: Optional[int] = None,
+                                     offset: Optional[int] = None):
+        """Synchronously get chapters of an audiobook"""
         params = EndpointsBase.params_for({"market": market, "limit": limit, "offset": offset})
         return self.get_request(f"audiobooks/{id}/chapters{params}")
+
+    def get_audiobook_chapters(self, id: str, market: Optional[MARKET] = None, limit: Optional[int] = None,
+                               offset: Optional[int] = None):
+        """Get chapters of an audiobook"""
+        if asyncio.get_event_loop().is_running():
+            return asyncio.ensure_future(self._get_audiobook_chapters_async(id, market, limit, offset))
+        else:
+            return self._get_audiobook_chapters_sync(id, market, limit, offset)
